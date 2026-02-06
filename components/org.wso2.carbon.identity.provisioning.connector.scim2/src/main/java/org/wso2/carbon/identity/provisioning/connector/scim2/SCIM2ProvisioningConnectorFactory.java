a6ffbee8770363bc07f359b240b9b56195353bf1
Pasindu Yeshan <pasinduyeshan98@gmail.com>
2026-02-06 20:33:06 +0530

    Add support for multiple authentication types

--- components/org.wso2.carbon.identity.provisioning.connector.scim2/src/main/java/org/wso2/carbon/identity/provisioning/connector/scim2/SCIM2ProvisioningConnectorFactory.java
+++ components/org.wso2.carbon.identity.provisioning.connector.scim2/src/main/java/org/wso2/carbon/identity/provisioning/connector/scim2/SCIM2ProvisioningConnectorFactory.java
@@ -1,17 +1,19 @@
 /*
- * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2018-2026, WSO2 LLC. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.identity.provisioning.connector.scim2;
@@ -23,6 +25,7 @@ import org.wso2.carbon.identity.application.common.model.SubProperty;
 import org.wso2.carbon.identity.provisioning.AbstractOutboundProvisioningConnector;
 import org.wso2.carbon.identity.provisioning.AbstractProvisioningConnectorFactory;
 import org.wso2.carbon.identity.provisioning.IdentityProvisioningException;
+import org.wso2.scim2.util.AuthenticationType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -75,39 +78,82 @@ public class SCIM2ProvisioningConnectorFactory extends AbstractProvisioningConne
 
         List<Property> properties = new ArrayList<Property>();
 
+        // Authentication Mode Property with SubProperties.
+        Property authMode = new Property();
+        authMode.setName(SCIM2ProvisioningConnectorConstants.SCIM2_AUTHENTICATION_MODE);
+        authMode.setDisplayName("Authentication Mode");
+        authMode.setDisplayOrder(1);
+        authMode.setRequired(true);
+        authMode.setType("enum");
+        authMode.setOptions(new String[]{
+                AuthenticationType.BASIC.getValue(),
+                AuthenticationType.BEARER.getValue(),
+                AuthenticationType.API_KEY.getValue(),
+                AuthenticationType.NONE.getValue()
+        });
+        authMode.setDefaultValue(AuthenticationType.BASIC.getValue());
+
+        // BASIC auth subproperties
         Property username = new Property();
         username.setName(SCIM2ProvisioningConnectorConstants.SCIM2_USERNAME);
         username.setDisplayName("Username");
-        username.setDisplayOrder(1);
-        username.setRequired(true);
+        username.setDisplayOrder(2);
+        username.setRequired(false);
         username.setType("string");
 
         Property userPassword = new Property();
         userPassword.setName(SCIM2ProvisioningConnectorConstants.SCIM2_PASSWORD);
         userPassword.setDisplayName("Password");
         userPassword.setConfidential(true);
-        userPassword.setDisplayOrder(2);
-        userPassword.setRequired(true);
+        userPassword.setDisplayOrder(3);
+        username.setRequired(false);
         userPassword.setType("string");
+        userPassword.setConfidential(true);
+
+        // BEARER auth subproperty
+        Property accessToken = new Property();
+        accessToken.setName(SCIM2ProvisioningConnectorConstants.SCIM2_ACCESS_TOKEN);
+        accessToken.setDisplayName("Access Token");
+        accessToken.setDisplayOrder(4);
+        accessToken.setRequired(false);
+        accessToken.setType("string");
+        accessToken.setConfidential(true);
+
+        // API_KEY auth subproperties
+        Property apiKeyHeader = new Property();
+        apiKeyHeader.setName(SCIM2ProvisioningConnectorConstants.SCIM2_API_KEY_HEADER);
+        apiKeyHeader.setDisplayName("API Key Header Name");
+        apiKeyHeader.setDisplayOrder(5);
+        apiKeyHeader.setRequired(false);
+        apiKeyHeader.setType("string");
+        apiKeyHeader.setConfidential(false);
+
+        Property apiKeyValue = new Property();
+        apiKeyValue.setName(SCIM2ProvisioningConnectorConstants.SCIM2_API_KEY_VALUE);
+        apiKeyValue.setDisplayName("API Key Value");
+        apiKeyValue.setDisplayOrder(6);
+        apiKeyValue.setRequired(false);
+        apiKeyValue.setType("string");
+        apiKeyValue.setConfidential(true);
 
         Property userEndpoint = new Property();
         userEndpoint.setName(SCIM2ProvisioningConnectorConstants.SCIM2_USER_EP);
         userEndpoint.setDisplayName("User Endpoint");
-        userEndpoint.setDisplayOrder(3);
+        userEndpoint.setDisplayOrder(7);
         userEndpoint.setRequired(true);
         userEndpoint.setType("string");
 
         Property groupEndpoint = new Property();
         groupEndpoint.setName(SCIM2ProvisioningConnectorConstants.SCIM2_GROUP_EP);
         groupEndpoint.setDisplayName("Group Endpoint");
-        groupEndpoint.setDisplayOrder(4);
+        groupEndpoint.setDisplayOrder(8);
         groupEndpoint.setType("string");
         groupEndpoint.setRequired(false);
 
         Property userStoreDomain = new Property();
         userStoreDomain.setName(SCIM2ProvisioningConnectorConstants.SCIM2_USERSTORE_DOMAIN);
         userStoreDomain.setDisplayName("User Store Domain");
-        userStoreDomain.setDisplayOrder(5);
+        userStoreDomain.setDisplayOrder(9);
         userStoreDomain.setRequired(false);
         userStoreDomain.setType("string");
 
@@ -115,7 +161,7 @@ public class SCIM2ProvisioningConnectorFactory extends AbstractProvisioningConne
         passwordProvisioning.setName(SCIM2ProvisioningConnectorConstants.SCIM2_ENABLE_PASSWORD_PROVISIONING);
         passwordProvisioning.setDisplayName("Enable Password Provisioning");
         passwordProvisioning.setDescription("Enable User password provisioning to a SCIM2 domain");
-        passwordProvisioning.setDisplayOrder(6);
+        passwordProvisioning.setDisplayOrder(10);
         passwordProvisioning.setRequired(false);
         passwordProvisioning.setType("boolean");
         passwordProvisioning.setDefaultValue("true");
@@ -123,14 +169,18 @@ public class SCIM2ProvisioningConnectorFactory extends AbstractProvisioningConne
         SubProperty defaultPassword = new SubProperty();
         defaultPassword.setName(SCIM2ProvisioningConnectorConstants.SCIM2_DEFAULT_PASSWORD);
         defaultPassword.setDisplayName("Default Password");
-        defaultPassword.setDisplayOrder(7);
+        defaultPassword.setDisplayOrder(11);
         defaultPassword.setRequired(false);
         defaultPassword.setType("string");
         defaultPassword.setConfidential(true);
         passwordProvisioning.setSubProperties(new SubProperty[] {defaultPassword});
 
+        properties.add(authMode);
         properties.add(username);
         properties.add(userPassword);
+        properties.add(accessToken);
+        properties.add(apiKeyHeader);
+        properties.add(apiKeyValue);
         properties.add(userEndpoint);
         properties.add(groupEndpoint);
         properties.add(userStoreDomain);
--- components/org.wso2.carbon.identity.provisioning.connector.scim2/src/test/java/org.wso2.carbon.identity.provisioning.connector.scim2.test/SCIM2ProvisioningConnectorFactoryTest.java
+++ components/org.wso2.carbon.identity.provisioning.connector.scim2/src/test/java/org.wso2.carbon.identity.provisioning.connector.scim2.test/SCIM2ProvisioningConnectorFactoryTest.java
@@ -1,47 +1,43 @@
 /*
- * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2018-2026, WSO2 LLC. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 package org.wso2.carbon.identity.provisioning.connector.scim2.test;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.wso2.carbon.identity.application.common.model.Property;
-import org.wso2.carbon.identity.application.common.model.SubProperty;
+import org.wso2.carbon.identity.provisioning.AbstractOutboundProvisioningConnector;
+import org.wso2.carbon.identity.provisioning.connector.scim2.SCIM2ProvisioningConnectorConstants;
 import org.wso2.carbon.identity.provisioning.connector.scim2.SCIM2ProvisioningConnectorFactory;
+import org.wso2.scim2.util.AuthenticationType;
 
-import java.util.ArrayList;
+import java.lang.reflect.Method;
 import java.util.List;
-
-import static org.mockito.MockitoAnnotations.initMocks;
 
 public class SCIM2ProvisioningConnectorFactoryTest {
 
     private SCIM2ProvisioningConnectorFactory scim2ProvisioningConnectorFactory;
 
     @BeforeMethod
-    public void setUp() throws Exception {
+    public void setUp() {
 
         scim2ProvisioningConnectorFactory = new SCIM2ProvisioningConnectorFactory();
-        initMocks(this);
-    }
-
-    @AfterMethod
-    public void tearDown() throws Exception {
-
     }
 
     @Test
@@ -54,22 +50,233 @@ public class SCIM2ProvisioningConnectorFactoryTest {
     @Test
     public void testGetConfigurationProperties() {
 
-        List<Property> configProperties = new ArrayList<Property>();
-        Property username = new Property();
-        configProperties.add(username);
-        Property userPassword = new Property();
-        configProperties.add(userPassword);
-        Property userEndpoint = new Property();
-        configProperties.add(userEndpoint);
-        Property groupEndpoint = new Property();
-        configProperties.add(groupEndpoint);
-        Property userStoreDomain = new Property();
-        configProperties.add(userStoreDomain);
-        Property passwordProvisioning = new Property();
-        SubProperty defaultPassword = new SubProperty();
-        passwordProvisioning.setSubProperties(new SubProperty[] {defaultPassword});
-        configProperties.add(passwordProvisioning);
-        Assert.assertEquals(configProperties.size(), scim2ProvisioningConnectorFactory.getConfigurationProperties().
-                size());
+        // Get configuration properties.
+        List<Property> properties = scim2ProvisioningConnectorFactory.getConfigurationProperties();
+
+        // Verify properties are returned.
+        Assert.assertNotNull(properties);
+        Assert.assertEquals(properties.size(), 10, "Should have 10 configuration properties");
+    }
+
+    @Test
+    public void testBuildConnectorWithBasicAuth() throws Exception {
+
+        // Create properties for BASIC authentication.
+        // Properties are already decrypted at IDP DAO layer.
+        Property[] properties = createBasicAuthPropertiesWithPlainValues("testUser", "testPassword");
+
+        AbstractOutboundProvisioningConnector connector = invokeBuildConnector(properties);
+
+        // Verify connector was created.
+        Assert.assertNotNull(connector);
+    }
+
+    @Test
+    public void testBuildConnectorWithBearerAuth() throws Exception {
+
+        // Create properties for BEARER authentication.
+        // Properties are already decrypted at IDP DAO layer.
+        Property[] properties = createBearerAuthPropertiesWithPlainValues("test-bearer-token");
+
+        AbstractOutboundProvisioningConnector connector = invokeBuildConnector(properties);
+
+        // Verify connector was created.
+        Assert.assertNotNull(connector);
+    }
+
+    @Test
+    public void testBuildConnectorWithApiKeyAuth() throws Exception {
+
+        // Create properties for API_KEY authentication.
+        // Properties are already decrypted at IDP DAO layer.
+        Property[] properties = createApiKeyAuthPropertiesWithPlainValues("X-API-Key", "test-api-key");
+
+        AbstractOutboundProvisioningConnector connector = invokeBuildConnector(properties);
+
+        // Verify connector was created.
+        Assert.assertNotNull(connector);
+    }
+
+    @Test
+    public void testGetConfigurationPropertiesContainsAllAuthTypes() {
+
+        // Get configuration properties.
+        List<Property> properties = scim2ProvisioningConnectorFactory.getConfigurationProperties();
+
+        // Verify properties are returned.
+        Assert.assertNotNull(properties);
+        Assert.assertTrue(properties.size() > 0, "Configuration properties should not be empty.");
+
+        // Verify authentication mode property exists.
+        Property authModeProperty = findPropertyByName(properties, SCIM2ProvisioningConnectorConstants.SCIM2_AUTHENTICATION_MODE);
+        Assert.assertNotNull(authModeProperty, "Authentication mode property should exist.");
+        Assert.assertEquals(authModeProperty.getType(), "enum", "Authentication mode should be enum type.");
+        Assert.assertNotNull(authModeProperty.getOptions(), "Authentication mode should have options.");
+
+        // Verify all 4 auth types are present.
+        String[] options = authModeProperty.getOptions();
+        Assert.assertTrue(containsOption(options, AuthenticationType.BASIC.getValue()),
+                "Should contain BASIC auth type.");
+        Assert.assertTrue(containsOption(options, AuthenticationType.BEARER.getValue()),
+                "Should contain BEARER auth type.");
+        Assert.assertTrue(containsOption(options, AuthenticationType.API_KEY.getValue()),
+                "Should contain API_KEY auth type.");
+        Assert.assertTrue(containsOption(options, AuthenticationType.NONE.getValue()),
+                "Should contain NONE auth type.");
+    }
+
+    /**
+     * Helper method to create BASIC authentication properties with plain values.
+     * Properties are already decrypted at IDP DAO layer.
+     *
+     * @param username Username value.
+     * @param password Password value.
+     * @return Array of properties.
+     */
+    private Property[] createBasicAuthPropertiesWithPlainValues(String username, String password) {
+
+        Property[] properties = new Property[3];
+
+        // Auth mode.
+        Property authMode = new Property();
+        authMode.setName(SCIM2ProvisioningConnectorConstants.SCIM2_AUTHENTICATION_MODE);
+        authMode.setValue(AuthenticationType.BASIC.getValue());
+        properties[0] = authMode;
+
+        // Username.
+        Property usernameProperty = new Property();
+        usernameProperty.setName(SCIM2ProvisioningConnectorConstants.SCIM2_USERNAME);
+        usernameProperty.setValue(username);
+        usernameProperty.setConfidential(true);
+        properties[1] = usernameProperty;
+
+        // Password.
+        Property passwordProperty = new Property();
+        passwordProperty.setName(SCIM2ProvisioningConnectorConstants.SCIM2_PASSWORD);
+        passwordProperty.setValue(password);
+        passwordProperty.setConfidential(true);
+        properties[2] = passwordProperty;
+
+        return properties;
+    }
+
+    /**
+     * Helper method to create BEARER authentication properties with plain values.
+     * Properties are already decrypted at IDP DAO layer.
+     *
+     * @param accessToken Access token value.
+     * @return Array of properties.
+     */
+    private Property[] createBearerAuthPropertiesWithPlainValues(String accessToken) {
+
+        Property[] properties = new Property[2];
+
+        // Auth mode.
+        Property authMode = new Property();
+        authMode.setName(SCIM2ProvisioningConnectorConstants.SCIM2_AUTHENTICATION_MODE);
+        authMode.setValue(AuthenticationType.BEARER.getValue());
+        properties[0] = authMode;
+
+        // Access token.
+        Property accessTokenProperty = new Property();
+        accessTokenProperty.setName(SCIM2ProvisioningConnectorConstants.SCIM2_ACCESS_TOKEN);
+        accessTokenProperty.setValue(accessToken);
+        accessTokenProperty.setConfidential(true);
+        properties[1] = accessTokenProperty;
+
+        return properties;
+    }
+
+    /**
+     * Helper method to create API_KEY authentication properties with plain values.
+     * Properties are already decrypted at IDP DAO layer.
+     *
+     * @param header API key header name.
+     * @param value API key value.
+     * @return Array of properties.
+     */
+    private Property[] createApiKeyAuthPropertiesWithPlainValues(String header, String value) {
+
+        Property[] properties = new Property[3];
+
+        // Auth mode.
+        Property authMode = new Property();
+        authMode.setName(SCIM2ProvisioningConnectorConstants.SCIM2_AUTHENTICATION_MODE);
+        authMode.setValue(AuthenticationType.API_KEY.getValue());
+        properties[0] = authMode;
+
+        // API key header.
+        Property headerProperty = new Property();
+        headerProperty.setName(SCIM2ProvisioningConnectorConstants.SCIM2_API_KEY_HEADER);
+        headerProperty.setValue(header);
+        headerProperty.setConfidential(false);
+        properties[1] = headerProperty;
+
+        // API key value.
+        Property valueProperty = new Property();
+        valueProperty.setName(SCIM2ProvisioningConnectorConstants.SCIM2_API_KEY_VALUE);
+        valueProperty.setValue(value);
+        valueProperty.setConfidential(true);
+        properties[2] = valueProperty;
+
+        return properties;
+    }
+
+    /**
+     * Helper method to find a property by name.
+     *
+     * @param properties List of properties.
+     * @param name Property name to find.
+     * @return Property if found, null otherwise.
+     */
+    private Property findPropertyByName(List<Property> properties, String name) {
+
+        if (properties == null) {
+            return null;
+        }
+
+        for (Property property : properties) {
+            if (property.getName().equals(name)) {
+                return property;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Helper method to check if an option exists in options array.
+     *
+     * @param options Array of options.
+     * @param option Option to check.
+     * @return True if option exists, false otherwise.
+     */
+    private boolean containsOption(String[] options, String option) {
+
+        if (options == null || option == null) {
+            return false;
+        }
+
+        for (String opt : options) {
+            if (option.equals(opt)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Helper method to invoke buildConnector method using reflection.
+     *
+     * @param properties Properties to pass to buildConnector.
+     * @return AbstractOutboundProvisioningConnector instance.
+     * @throws Exception If invocation fails.
+     */
+    private AbstractOutboundProvisioningConnector invokeBuildConnector(Property[] properties) throws Exception {
+
+        Method buildConnectorMethod = SCIM2ProvisioningConnectorFactory.class
+                .getDeclaredMethod("buildConnector", Property[].class);
+        buildConnectorMethod.setAccessible(true);
+        return (AbstractOutboundProvisioningConnector) buildConnectorMethod
+                .invoke(scim2ProvisioningConnectorFactory, (Object) properties);
     }
 }
